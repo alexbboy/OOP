31ecf3f8a6673e18c629f571026cd33fdb945b0e
alexbboy <46032273+alexbboy@users.noreply.github.com>
2020-11-11 14:28:34 +0300

    5 лаба

--- Main.java
+++ Main.java
@@ -1,197 +1,70 @@
 package com.company;
-import java.util.Scanner;
+
+
+import java.awt.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите размер матрицы:");
-        int m = in.nextInt(), n = in.nextInt();
-        System.out.println("Введите номер операции:" + "\n"
-                + "1) вывод элемента" + "\n"
-                + "2) изменение элемента" + "\n"
-                + "3) вывод резмерностей" + "\n"
-                + "4) сложение матриц" + "\n"
-                + "5) вычитание матриц" + "\n"
-                + "6) умножение матриц" + "\n"
-                + "7) умножение на скаляр" + "\n"
-                + "8) вычисление определителя" + "\n"
-                + "9) сравнение матриц" + "\n"
-                + "10) вывод матрицы" + "\n");
-        int var = in.nextInt();
 
-        Matrix matrixA = new Matrix(m, n);
-        Matrix matrixB = new Matrix(m, n);
-        Matrix matrixC = new Matrix(m, n);
-
-        System.out.println("Введите первую матрицу:");
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int inp = in.nextInt();
-                matrixA.put(i, j, inp);
-            }
+        List<Shape> shapes = new ArrayList<>();
+        double sum = 0;
+        shapes.add(new Round(7));
+        shapes.add(new Round(5));
+        shapes.add(new Triangle(3, 6, 8));
+        shapes.add(new Triangle(2,3,4));
+        shapes.add(new Rectangle(10, 5));
+        shapes.add(new Rectangle(12,4));
+        shapes.add(new Square(6));
+        shapes.add(new Square(12));
+        for (int i = 0; i < shapes.size(); i++) {
+            sum = sum + shapes.get(i).calcArea();
         }
-
-        System.out.println("Введите вторую матрицу:");
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int inp = in.nextInt();
-                matrixB.put(i, j, inp);
-            }
-        }
-
-        int i, j, value;
-
-        switch (var) {
-            case (1) -> {
-                System.out.println("Введите номер элемента:");
-                i = in.nextInt();
-                j = in.nextInt();
-                System.out.println("Элеменет номер [" + i + "][" + j + "] равен " + matrixA.get(i, j));
-            }
-            case (2) -> {
-                System.out.println("Введите номер элемента:");
-                i = in.nextInt();
-                j = in.nextInt();
-                System.out.println("Введите новое значение:");
-                value = in.nextInt();
-                matrixA.put(i, j, value);
-            }
-            case (3) -> System.out.println("размерность матрицы:" + matrixA.getM() + " на " + matrixA.getN());
-            case (4) -> matrixA.sum(matrixB);
-            case (5) -> matrixA.dif(matrixB);
-            case (6) -> matrixC.mul(matrixB);
-            case (7) -> {
-                value = in.nextInt();
-                for (i = 1; i < m; i++) {
-                    for (j = 1; i < n; i++) {
-                        matrixA.mulsk(i, j, value);
-                    }
-                }
-            }
-            case (8) -> matrixA.det(0);
-            case (9) -> matrixA.equals(matrixB);
-            case (10) -> {
-                System.out.println("Матрица A:" + "\n" + matrixA.toString());
-                System.out.println("Матрица B:" + "\n" + matrixB.toString());
-                System.out.println("Матрица C:" + "\n" + matrixC.toString());
-            }
-        }
-    }
+        System.out.println("Sum= " + sum);
+SerializeAll Save=new SerializeAll();
+Save.save(shapes);
+List<Shape> shapes1=new ArrayList<>();
+shapes1=Save.load();
+for(int i=0;i<shapes1.size();i++){
+    System.out.println(shapes1.get(i).toString());
+}
 }
 
-class Matrix {
-    private int[][] matrixTable;
-    private int m, n;
 
-    public Matrix(int m, int n) {
-        this.matrixTable = new int[m][n];
-        this.m = m;
-        this.n = n;
-    }
 
-    public int getM() { return m; }
-    public int getN() { return n; }
-
-    public int get(int i, int j) {
-        return this.matrixTable[i][j];
-    }
-
-    public void put(int i, int j, int value) {
-        this.matrixTable[i][j] = value;
-    }
-
-    public Matrix sum(Matrix b) {
-        Matrix c = new Matrix(m, n);
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                c.put(i, j, this.get(i, j) + b.get(i, j));
+    public void max(List<Shape> object) {
+        int maximum = 0;
+        int minimum = 0;
+        int maximumPer = 0;
+        int minimumPer = 0;
+        double maxPer = 0;
+        double minPer = Double.MAX_VALUE;
+        double maxim = 0;
+        double min = Double.MAX_VALUE;
+        for (int i = 0; i < object.size(); i++) {
+            if (object.get(i).calcArea() > maxim) {
+                maxim = object.get(i).calcArea();
+                maximum = i;
+            }
+            if (object.get(i).calcArea() < min) {
+                min = object.get(i).calcArea();
+                minimum = i;
+            }
+            if (object.get(i).calcPerimetr() > maxPer) {
+                maxPer = object.get(i).calcPerimetr();
+                maximumPer = i;
+            }
+            if (object.get(i).calcPerimetr() < minPer) {
+                minPer = object.get(i).calcPerimetr();
+                minimumPer = i;
             }
         }
-        return c;
-    }
-
-    public Matrix dif(Matrix b) {
-        Matrix c = new Matrix(m, n);
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                c.put(i, j, this.get(i, j) - b.get(i, j));
-            }
-        }
-        return c;
-    }
-
-    public Matrix mul(Matrix b) {
-        int value = 0;
-        Matrix c = new Matrix(m, n);
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                for (int k = 0; k < n; k++)
-                {
-                    value += this.get(i, j) * b.get(i, j);
-                }
-                c.put(i, j, value);
-            }
-        }
-        return c;
-    }
-
-    public void mulsk(int i, int j, int value) {
-        this.matrixTable[i][j] *= value;
-    }
-
-    public int det(int j) {
-        int det = 0;
-        if (n != m)
-            System.out.println("Ошибка, не квадратная матрица");
-        if (n == 1)
-            det = this.matrixTable[0][0];
-        if (n == 2)
-            det = this.matrixTable[0][0] * this.matrixTable[1][1] - this.matrixTable[1][0] * this.matrixTable[0][1];
-        if (n > 2)
-        {
-            for (int i = j; i < n; i++)
-            {
-                det += this.matrixTable[1][i] * det(j + 1);;
-            }
-        }
-        return det;
-    }
-
-    @Override
-    public boolean equals(Object b) {
-        if (b == null || !(b instanceof Matrix)) {
-            return false;
-        }
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (this != b)
-                {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        String strMatrix = "";
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                strMatrix += this.matrixTable[i][j] + " ";
-            }
-            strMatrix += "\n";
-        }
-        return strMatrix;
+        System.out.println("Maximum area= " + object.get(maximum).toString());
+        System.out.println("Minimum area= " + object.get(minimum).toString());
+        System.out.println("Maximum perimetr= " + object.get(maximumPer).toString());
+        System.out.println("Minimum perimetr= " + object.get(minimumPer).toString());
     }
 }
--- Rectangle.java
+++ Rectangle.java
@@ -14,43 +14,6 @@ public class Rectangle implements Shape, Serializable {
         this.width=width;
     }
 
-    public void save(List<Shape> list) {
-        try {
-            FileOutputStream file = new FileOutputStream("C:\\Users\\Alex\\Desktop\\Save.json");
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(file);
-            objectOutputStream.writeObject(list);
-            objectOutputStream.close();
-        }
-        catch(FileNotFoundException d)
-        {
-            throw new RuntimeException("File dont found");
-
-        }
-        catch(IOException d){
-            throw new RuntimeException("Exception");
-        }
-    }
-
-    public List<Shape> load(){
-        List<Shape> newlist = new ArrayList<>();
-        try {
-            FileInputStream file = new FileInputStream("C:\\Users\\Alex\\Desktop\\Save.json");
-            ObjectInputStream objectOutputStream = new ObjectInputStream(file);
-            newlist=(List<Shape>) objectOutputStream.readObject();
-        }
-        catch(FileNotFoundException d)
-        {
-            throw new RuntimeException("File dont found");
-
-        }
-        catch(IOException d){
-            throw new RuntimeException("Exception");
-        }
-        catch (ClassNotFoundException d){
-            throw new RuntimeException("Class not found");
-        }
-        return newlist;
-    }
 
 
     public double getHight() {
